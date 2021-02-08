766bf6473e0f0dacf31eb4a3d5f09060871cef6c
Alessandro Liggieri <smmb@live.it>
2021-02-08 16:13:33 +0100

    Esercizio completo (bonus incluso)

--- main.js
+++ main.js
@@ -4,22 +4,12 @@ new Vue({
     listaDischi: [],
     selected: ''
   },
-  methods: {
-    select: function () {
-      this.listaDischi.filter((item, i) => {
-        return this.selected == item.genre;
-      });
-
-    }
-  },
   mounted() {
     const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(function(resp) {
       self.listaDischi = resp.data.response;
     });
-
   }
-
 });
 Vue.config.devtools = true;
